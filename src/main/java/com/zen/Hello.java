8cba3b9b9c358ec32ac98f4b6c5319f1b91543b9
mathapelo-motloung <mathapelo886@gmil.com>
2021-01-21 20:55:47 +0200

    updated hello.java

--- src/main/java/com/zen/Hello.java
+++ src/main/java/com/zen/Hello.java
@@ -3,8 +3,6 @@ package com.zen;
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("Hello Again");
-		System.out.println("Hello Again 3");
-		System.out.println("Hello Again 4");
+		
 	}
 }
