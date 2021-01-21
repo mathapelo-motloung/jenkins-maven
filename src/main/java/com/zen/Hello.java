dd4308d7f1d5890379a05ac1be89499a11d5f9eb
mathapelo-motloung <mathapelo886@gmil.com>
2021-01-21 21:11:36 +0200

    final commit

--- src/main/java/com/zen/Hello.java
+++ src/main/java/com/zen/Hello.java
@@ -2,8 +2,8 @@ package com.zen;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("It has to work");
-		System.out.println("work");
+		System.out.println("It works");
+
 		
 	}
 }
